5f595f6bf042fa80936359ab85f8cf018ae02952
dkseoals15 <aa88491231@gmail.com>
2026-05-06 16:52:48 +0900

    git 연결

---
+++ src/main/java/com/daemin/clean_talk/CleanTalkApplication.java
@@ -0,0 +1,13 @@
+package com.daemin.clean_talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanTalkApplication.class, args);
+	}
+
+}
